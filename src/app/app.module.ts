a7cf7f770e44f5a6c37429b120d821b42837f614
Kathleen Aranzaso <gaiaaranzaso@live.com>
2022-02-16 14:14:17 +0100

    assignment 2: add designsystem folder and move component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './designsystem/footer/footer.component';
 
 @NgModule({
   declarations: [
