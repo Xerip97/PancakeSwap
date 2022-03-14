52aaa0c6bf38bf2a1dca7f880e61e96e9d7f0f18
Fabio Viola <fabioviola97@gmail.com>
2022-03-14 19:31:51 +0100

    Fix previous commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FooterComponent } from './designsystem/footer/footer.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
 import { FormsContainerComponent } from './designsystem/forms-container/forms-container.component';
 import { FormDropdownComponent } from './designsystem/forms-container/form-dropdown/form-dropdown.component';
 import { FormTextSearchComponent } from './designsystem/forms-container/form-text-search/form-text-search.component';
 import { FormPollComponent } from './designsystem/forms-container/form-poll/form-poll.component';
 import { FormSliderComponent } from './designsystem/forms-container/form-slider/form-slider.component';
 import { FormTextTradeComponent } from './designsystem/forms-container/form-text-trade/form-text-trade.component';
-import { NavbarComponent } from './designsystem/navbar/navbar/navbar.component';
+import { NavbarComponent } from './core/layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -21,12 +21,10 @@ import { NavbarComponent } from './designsystem/navbar/navbar/navbar.component';
     FormPollComponent,
     FormSliderComponent,
     FormTextTradeComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
