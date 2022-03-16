106ca4909f2b1f8a7d5d89334e64a23bec326824
[Marco] <[marco.giannola97@gmail.com]>
2022-03-16 13:55:32 +0100

    Buttons 0.5 alpha preview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './designsystem/footer/footer.component';
+import { BtnTrisComponent } from './btn-tris/btn-tris.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    BtnTrisComponent
   ],
   imports: [
     BrowserModule
