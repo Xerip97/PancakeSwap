245b82a6953161dd312b79adbc535d133ca615c6
[Marco] <[marco.giannola97@gmail.com]>
2022-03-22 15:15:27 +0100

    Liquidity 0.5

---
+++ src/app/core/layout/trade-undernavbar-div/trade-undernavbar-div.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pancakeswap-trade-undernavbar-div',
+  templateUrl: './trade-undernavbar-div.component.html',
+  styleUrls: ['./trade-undernavbar-div.component.css']
+})
+export class TradeUndernavbarDivComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
