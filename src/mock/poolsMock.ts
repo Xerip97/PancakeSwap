a9f3ace529c8cab9661b54db04c7d9be2f63a792
Fabio Viola <fabioviola97@gmail.com>
2022-03-22 16:53:00 +0100

    Assignment 51: add Pools model and pools mock-data

---
+++ src/mock/poolsMock.ts
@@ -0,0 +1,138 @@
+import { Pool } from '@app/models/Pool';
+
+export const pools: Pool[] = [
+  {
+    id: 0,
+    type: 'IFO',
+    name: 'CAKE',
+    description: 'Stake CAKE to participate in IFOs',
+    urlIcon:
+      'https://pancakeswap.finance/images/tokens/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82.svg',
+    urlIcon2: 'https://pancakeswap.finance/images/tokens/ifo-pool-icon.svg',
+    apr: 48.71,
+    apy: 61.45,
+    totalStaked: 29462078,
+    earned: 0,
+    auto: true,
+    creditCalculation: 15954340,
+    unstakingFee: 1,
+    performanceFee: 2,
+    daylyCompound: 1,
+  },
+  {
+    id: 1,
+    type: 'Auto',
+    name: 'CAKE',
+    description: 'Automatic restaking ',
+    urlIcon:
+      'https://pancakeswap.finance/images/tokens/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82.svg',
+    urlIcon2: 'https://pancakeswap.finance/images/tokens/autorenew.svg',
+    apr: 48.71,
+    apy: 61.51,
+    totalStaked: 132212784,
+    earned: 0,
+    auto: true,
+    unstakingFee: 0.1,
+    performanceFee: 2,
+    daylyCompound: 5000,
+  },
+  {
+    id: 2,
+    type: 'Manual',
+    name: 'CAKE',
+    description: 'Earn CAKE, stake CAKE',
+    urlIcon:
+      'https://pancakeswap.finance/images/tokens/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82.svg',
+    urlIcon2:
+      'https://pancakeswap.finance/images/tokens/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82.svg',
+    apr: 48.71,
+    apy: 62.71,
+    totalStaked: 54189742,
+    earned: 0,
+    auto: false,
+    daylyCompound: 1,
+  },
+  {
+    id: 3,
+    type: 'Manual',
+    name: 'BSW',
+    description: 'Stake CAKE',
+    urlIcon:
+      'https://pancakeswap.finance/images/tokens/0x965F527D9159dCe6288a2219DB51fc6Eef120dD1.svg',
+    urlIcon2:
+      'https://pancakeswap.finance/images/tokens/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82.svg',
+    apr: 56.56,
+    apy: 75.97,
+    totalStaked: 2481216,
+    earned: 0,
+    endsIn: 1494165,
+    auto: false,
+    daylyCompound: 1,
+  },
+  {
+    id: 4,
+    type: 'Manual',
+    name: 'DUET',
+    description: 'Stake CAKE',
+    urlIcon:
+      'https://pancakeswap.finance/images/tokens/0x95EE03e1e2C5c4877f9A298F1C0D6c98698FAB7B.svg',
+    urlIcon2:
+      'https://pancakeswap.finance/images/tokens/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82.svg',
+    apr: 57.54,
+    apy: 77.65,
+    totalStaked: 391453,
+    earned: 0,
+    endsIn: 1406881,
+    auto: false,
+    daylyCompound: 1,
+  },
+  {
+    id: 5,
+    type: 'Manual',
+    name: 'GMT',
+    description: 'Stake CAKE',
+    urlIcon:
+      'https://pancakeswap.finance/images/tokens/0x3019BF2a2eF8040C242C9a4c5c4BD4C81678b2A1.svg',
+    urlIcon2:
+      'https://pancakeswap.finance/images/tokens/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82.svg',
+    apr: 59.56,
+    apy: 81.32,
+    totalStaked: 9082292,
+    earned: 0,
+    endsIn: 1352192,
+    auto: false,
+    daylyCompound: 1,
+  },
+  {
+    id: 6,
+    type: 'Manual',
+    name: 'ACH',
+    description: 'Stake CAKE',
+    urlIcon:
+      'https://pancakeswap.finance/images/tokens/0xBc7d6B50616989655AfD682fb42743507003056D.svg',
+    urlIcon2:
+      'https://pancakeswap.finance/images/tokens/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82.svg',
+    apr: 0,
+    apy: 0,
+    totalStaked: 26199,
+    earned: 0,
+    endsIn: 0,
+    auto: false,
+  },
+  {
+    id: 7,
+    type: 'Manual',
+    name: 'FUND',
+    description: 'Stake CAKE',
+    urlIcon:
+      'https://pancakeswap.finance/images/tokens/0x477bC8d23c634C154061869478bce96BE6045D12.svg',
+    urlIcon2:
+      'https://pancakeswap.finance/images/tokens/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82.svg',
+    apr: 0,
+    apy: 0,
+    totalStaked: 24009,
+    earned: 0,
+    endsIn: 0,
+    auto: false,
+  },
+];
