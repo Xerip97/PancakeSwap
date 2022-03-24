7d92306b997d43454ceb4b481824b3f992c34a94
Kathleen Aranzaso <gaiaaranzaso@live.com>
2022-03-24 15:16:20 +0100

    assignment 49: add mock data farms

---
+++ src/app/models/Farms.ts
@@ -0,0 +1,11 @@
+export interface Farms {
+    id: number,
+    name: string,
+    urlAvatar: string,
+    urlIcon: string,
+    apr: number,
+    multiplier: number,
+    earned: number,
+    liquidity: number,
+    creation: Date
+}
--- src/app/pages/farms-container/farms-container.component.ts
+++ src/app/pages/farms-container/farms-container.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Farms } from '@app/models/Farms';
+import { farms } from 'src/mock/farmsMock';
 
 @Component({
   selector: 'pancakeswap-farms-container',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./farms-container.component.css'],
 })
 export class FarmsContainerComponent implements OnInit {
+  farms: Farms[] = farms
   constructor(
    
   ) {}
