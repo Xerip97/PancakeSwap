0f039d8826b119b360592df0f1651641e4c32936
Fabio Viola <fabioviola97@gmail.com>
2022-03-25 15:02:44 +0100

    Assignment 51: add bounty info on section header, and update classes' name

--- src/app/pages/pools-container/pools-container.component.ts
+++ src/app/pages/pools-container/pools-container.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'pancakeswap-pools-container',
   templateUrl: './pools-container.component.html',
-  styleUrls: ['./pools-container.component.css']
+  styleUrls: ['./pools-container.component.css'],
 })
 export class PoolsContainerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  showBountyInfo() {
+    const bountyInfo = document.querySelector('.bounty-info');
+    bountyInfo?.classList.add('bounty-info-visible');
   }
 
+  hideBountyInfo() {
+    const bountyInfo = document.querySelector('.bounty-info');
+    bountyInfo?.classList.remove('bounty-info-visible');
+  }
 }
