000e235174b4a5158727887a08e74792a7deaa29
Kathleen Aranzaso <gaiaaranzaso@live.com>
2022-04-07 15:14:18 +0200

    assignment 49: add logic show and hide details

--- src/app/pages/farms-container/farms-cards/farms-cards.component.ts
+++ src/app/pages/farms-container/farms-cards/farms-cards.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Farms } from '@app/models/Farms';
 
 @Component({
   selector: 'pancakeswap-farms-cards',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./farms-cards.component.css']
 })
 export class FarmsCardsComponent implements OnInit {
+  @Input() farms_item!: Farms
+  checked: boolean = false
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  changeClick(e: any) {
+    if (e.target.checked) {
+      return this.checked = true
+    }
+    else {
+      return this.checked = false
+    }
+  }
 }
