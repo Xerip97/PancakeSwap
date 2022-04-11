fcb2caa7e580fd415a2ad719dc7c4e274bd34181
Fabio Viola <92579721+Xerip97@users.noreply.github.com>
2022-04-11 23:06:53 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BtnTrisComponent } from './designsystem/buttons/btn-tris/btn-tris.component';
-import { FooterComponent } from './Core/layout/footer/footer.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
 import { FormsContainerComponent } from './designsystem/forms-container/forms-container.component';
 import { FormDropdownComponent } from './designsystem/forms-container/form-dropdown/form-dropdown.component';
 import { FormTextSearchComponent } from './designsystem/forms-container/form-text-search/form-text-search.component';
@@ -12,7 +12,7 @@ import { FormSliderComponent } from './designsystem/forms-container/form-slider/
 import { FormTextTradeComponent } from './designsystem/forms-container/form-text-trade/form-text-trade.component';
 import { FarmsContainerComponent } from './pages/farms-container/farms-container.component';
 import { TestComponent } from './pages/test/test.component';
-import {NavbarComponent} from './Core/layout/navbar/navbar.component';
+import {NavbarComponent} from './core/layout/navbar/navbar.component';
 import { LeaderboardContainerComponent } from './pages/leaderboard-container/leaderboard-container.component';
 import { TeamsComponent } from './pages/leaderboard-container/teams/teams.component';
 import { TeamContainerComponent } from './pages/leaderboard-container/team-container/team-container.component';
@@ -23,7 +23,7 @@ import { EarnUndernavbarDivComponent } from './core/earn-undernavbar-div/earn-un
 import { PoolsContainerComponent } from './pages/pools-container/pools-container.component';
 import { BountyInfoComponent } from './pages/pools-container/bounty-info/bounty-info.component';
 import { BtnDoubleComponent } from './designsystem/buttons/btn-double/btn-double.component';
-import { WarningBannerComponent } from './Core/warning-banner/warning-banner.component';
+import { WarningBannerComponent } from './core/warning-banner/warning-banner.component';
 import { DesignSystemComponent } from './pages/design-system/design-system.component';
 @NgModule({
   declarations: [
